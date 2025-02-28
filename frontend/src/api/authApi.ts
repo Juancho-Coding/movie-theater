d71cd421e3817a3697ed9386b6d31b37ac22d682
juan_pablo_alvarez <juanpabloalvarezovalle@gmail.com>
2025-02-28 11:25:35 -0500

    Implemented the user login and signup api

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -1,16 +1,24 @@
 import { userData } from "../context/AuthContext";
+import { BASEURL } from "./apiHelper";
 
 export async function makeLogin(
   email: string,
-  password: string
+  password: string,
+  dummy: boolean = false
 ): Promise<userData> {
-  //TODO implement the api for loging in
-  return {
-    name: "szdsadsadas",
-    email: email,
-    level: 2,
-    token: "3423423432",
-  };
+  const response = await fetch(`${BASEURL}/users/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password, dummy }),
+  });
+  if (!response.ok) {
+    const cause = await response.json();
+    throw new Error(cause.message);
+  }
+  const data: userData = await response.json();
+  return data;
 }
 
 export async function makeSignup(
@@ -18,6 +26,17 @@ export async function makeSignup(
   email: string,
   password: string
 ) {
-  // TODO implement the api for sigining up
-  return { status: true, message: "User created, please login" };
+  const response = await fetch(`${BASEURL}/users/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, email, password }),
+  });
+  if (!response.ok) {
+    const cause = await response.json();
+    throw new Error(cause.message);
+  }
+  const result = await response.json();
+  return { status: true, message: result.message };
 }
