7d2d83883b233a998564d14139457c9f5daa60c4
juan_pablo_alvarez <juanpabloalvarezovalle@gmail.com>
2025-03-05 07:18:58 -0500

    Added logic to make strong password validation on signup form

--- frontend/src/components/reservation/SignupForm.tsx
+++ frontend/src/components/reservation/SignupForm.tsx
@@ -2,16 +2,25 @@ import { Box, Button, Paper, TextField, Typography } from "@mui/material";
 
 import classes from "./form.module.css";
 import { useState } from "react";
+import { passwordValidator } from "../../utils/utils";
 
 const SignupForm = ({ signup, dismiss }: props) => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
+  // handle error in validation of password1
+  const [errorPass1, setErrorPass1] = useState<{
+    error: boolean;
+    msg: string;
+  } | null>(null);
+  // handle error in validation of password2
+  const [errorPass2, setErrorPass2] = useState<{
+    error: boolean;
+    msg: string;
+  } | null>(null);
+  // indicator that all fields are valid
   const [valid, setValid] = useState(false);
-
-  const error =
-    password.length > 0 && password2.length > 0 && password !== password2;
 
   const changeNameHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     const content = event.target.value;
@@ -41,23 +50,48 @@ const SignupForm = ({ signup, dismiss }: props) => {
     validateForm(name, email, password, content);
   };
 
+  // validate the data from the form
   const validateForm = (
     name: string,
     email: string,
     password: string,
     password2: string
   ) => {
+    console.log(passwordValidator(password));
+
+    if (passwordValidator(password)) {
+      setErrorPass1({ error: false, msg: "" });
+    } else {
+      setErrorPass1({
+        error: true,
+        msg: "The password must contain at least one number, one lowercase letter, one uppercase letter and one special character",
+      });
+    }
+    if (passwordValidator(password2)) {
+      setErrorPass2({ error: false, msg: "" });
+    } else {
+      setErrorPass2({
+        error: true,
+        msg: "The password must contain at least one number, one lowercase letter, one uppercase letter and one special character",
+      });
+    }
+    if (password !== password2) {
+      setErrorPass2({
+        error: true,
+        msg: "The passwords must match",
+      });
+    }
     setValid(
       name.length > 0 &&
         email.length > 0 &&
-        password.length > 0 &&
-        password2.length > 0
+        passwordValidator(password) &&
+        passwordValidator(password2) &&
+        password === password2
     );
   };
 
   const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // TODO Validate length and structure of password
     signup(name, email, password);
   };
 
@@ -102,8 +136,8 @@ const SignupForm = ({ signup, dismiss }: props) => {
             fullWidth
             value={password}
             onChange={changePasswordHandler}
-            error={error}
-            helperText={error ? "The password doesn't match" : ""}
+            error={errorPass1 ? errorPass1.error : false}
+            helperText={errorPass1 ? errorPass1.msg : ""}
           />
           <TextField
             id="password2"
@@ -114,8 +148,8 @@ const SignupForm = ({ signup, dismiss }: props) => {
             fullWidth
             value={password2}
             onChange={changePassword2Handler}
-            error={error}
-            helperText={error ? "The password doesn't match" : ""}
+            error={errorPass2 ? errorPass2.error : false}
+            helperText={errorPass2 ? errorPass2.msg : ""}
           />
         </Box>
         <Box className={classes["form-alternative-option"]}>
--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -2,3 +2,18 @@ export function dinamicImport(name: string) {
   const url = new URL(`../assets/${name}`, import.meta.url).href;
   return url;
 }
+
+/**
+ * Checks if the password is valid, must be between 6 and 20 characters, contain at least one number, one lowercase letter, one uppercase letter and one special character
+ * @param password string containing the password
+ * @returns
+ */
+export function passwordValidator(password: string): boolean {
+  const length = password.length >= 6 && password.length <= 20;
+  const hasNumber = /\d/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasSpecial = password.match(/[-_*+@]/) !== null;
+
+  return length && hasNumber && hasLowerCase && hasUpperCase && hasSpecial;
+}
