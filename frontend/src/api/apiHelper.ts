6d43098085c17aef4a91b40fabc4d5dec3b046d3
juan_pablo_alvarez <juanpabloalvarezovalle@gmail.com>
2025-03-24 19:41:48 -0500

    Fixed error class and added the socket url

--- frontend/src/api/apiHelper.ts
+++ frontend/src/api/apiHelper.ts
@@ -1,13 +1,14 @@
-export const BASEURL = "http://localhost:9001/api/v1";
+export const BASEURL = "http://localhost:9000/api/v1";
+export const BASE_IO_URL = "http://localhost:9000";
 
 export class ApiError extends Error {
   code: number;
-  status: string;
+  msg: string;
 
-  constructor(code: number, status: string) {
+  constructor(code: number, msg: string) {
     super();
     this.code = code;
-    this.status = status;
+    this.msg = msg;
   }
 }
 
