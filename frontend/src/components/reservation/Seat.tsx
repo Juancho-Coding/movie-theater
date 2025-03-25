6645bb9f832dbb09051016f6742da94af4d54bdd
juan_pablo_alvarez <juanpabloalvarezovalle@gmail.com>
2025-03-24 20:04:08 -0500

    Added a debouncer functionality to prevent modifying the status of seat too quickly

--- frontend/src/components/reservation/Seat.tsx
+++ frontend/src/components/reservation/Seat.tsx
@@ -1,8 +1,14 @@
 import { Box } from "@mui/material";
 
 import classes from "./Auditorium.module.css";
+import { useCallback, useRef, useState } from "react";
 
 const Seat = ({ row, column, status, onSelect }: props) => {
+  // store the flag for a debounce functionality
+  const [isDisabled, setIsDisabled] = useState(false);
+  // store the timeout ref for the debouncer
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
   const classStatus =
     status === 0
       ? "seat-available"
@@ -10,10 +16,23 @@ const Seat = ({ row, column, status, onSelect }: props) => {
       ? "seat-occupied"
       : "seat-selected";
 
-  const clickHandler = () => {
-    if (status === 1) return;
+  // handle when click on the seat
+  const clickHandler = useCallback(() => {
+    // seat is disabled or debounce is working
+    if (status === 1 || isDisabled) return;
+    // start debouncer
+    setIsDisabled(true);
     onSelect(row, column, status);
-  };
+    // debouncer timeout
+    const timeout = setTimeout(() => {
+      setIsDisabled(false);
+    }, 1000);
+    timeoutRef.current = timeout;
+    return () => {
+      // cancel the timeout
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    };
+  }, [isDisabled, status, row, column, onSelect]);
 
   return (
     <Box
