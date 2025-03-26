4e5221c57e68a4c7665ea5d92bbd52b751ed36bb
juan_pablo_alvarez <juanpabloalvarezovalle@gmail.com>
2025-03-26 16:33:38 -0500

    Changed the dummy user, info is retrieved form an api with name and email, inserting user into db is separated from controller, new dummy is stores as new user

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -31,15 +31,28 @@ export async function loginUser(
     //checks for validation error before continuing
     validationResults(req);
     if (dummy) {
-      const userid = randomUUID();
+      const userData = await getDummyUser();
+      if (!userData) {
+        res.status(500).json({ msg: "Internal error server" });
+        return;
+      }
+      const result = await saveNewUser(
+        userData.name,
+        userData.email,
+        userData.password
+      );
+      if (!result.result) {
+        res.status(500).json({ msg: "Internal error server" });
+        return;
+      }
       const jwt = jsonwebtoken.sign(
-        { userid: userid, email: "pedroperez@gmail.com" },
+        { userid: result.userId, email: userData.email },
         process.env.JWT_SECRET!,
         { expiresIn: "1h" }
       );
       res.status(200).json({
-        name: "Average Joe",
-        email: "averagejoe@average.com",
+        name: userData.name,
+        email: userData.email,
         level: 0,
         token: jwt,
       });
@@ -89,26 +102,72 @@ export async function signupUser(
   try {
     //checks for validation error before continuing
     validationResults(req);
-    // find user with tha same parameters
-    const existingUser = await dbQuery(`SELECT * FROM users WHERE email = $1`, [
-      email,
-    ]);
-    if (existingUser.rows.length > 0) {
+    const result = await saveNewUser(name, email, password);
+    if (!result.result) {
       res.status(400).json({ msg: "User already exists" });
       return;
     }
-    // hash password and generate a new user id
-    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
-    const newUserId = randomUUID();
-    // save new user in the database
-    await dbQuery(
-      `INSERT INTO users (name, email, password, userid) VALUES ($1, $2, $3, $4)`,
-      [name, email, hashedPassword, newUserId]
-    );
     res.status(200).json({ status: true, msg: `${name} was created` });
   } catch (error) {
     // checks if the error was a validation error
     if (validationResponse(error, res)) return;
     res.status(500).json({ msg: "Internal error server" });
+  }
+}
+
+/**
+ * Validates the user data, if correct insert it on DB
+ * @param name name of new user
+ * @param email email of new user
+ * @param password new password
+ * @returns boolean, true if success
+ */
+async function saveNewUser(name: string, email: string, password: string) {
+  // find user with tha same parameters
+  const existingUser = await dbQuery(`SELECT * FROM users WHERE email = $1`, [
+    email,
+  ]);
+  if (existingUser.rows.length > 0) {
+    return { result: false, userId: null };
+  }
+  // hash password and generate a new user id
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+  const newUserId = randomUUID();
+  // save new user in the database
+  await dbQuery(
+    `INSERT INTO users (name, email, password, userid) VALUES ($1, $2, $3, $4)`,
+    [name, email, hashedPassword, newUserId]
+  );
+  return { result: true, userId: newUserId };
+}
+
+/**
+ * Generate a dummy user with name and email to be used, from randomuser.me
+ * @returns user object with name, email and password or null if error ocurred
+ */
+async function getDummyUser() {
+  type dummy = {
+    results: {
+      name: { title: string; first: string; last: string };
+      email: string;
+    }[];
+    info: any;
+  };
+  try {
+    const result = await fetch("https://randomuser.me/api/?inc=name,email");
+    if (!result.ok) {
+      return null;
+    }
+    const dummyUser: dummy = await result.json();
+    return {
+      name: `${dummyUser.results[0].name.first} ${dummyUser.results[0].name.last}`,
+      email: dummyUser.results[0].email,
+      password: Array.from({ length: 10 }, () =>
+        Math.floor(Math.random() * 10)
+      ).join(""),
+    };
+  } catch (error) {
+    console.log(error);
+    return null;
   }
 }
