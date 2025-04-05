e71b86fb73a836511b58595cd184d5016a3db583
juan_pablo_alvarez <juanpabloalvarezovalle@gmail.com>
2025-04-05 14:49:30 -0500

    Added routes for payment and tickets

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import AuthRoutes from "./userRoutes";
 import moviesRoutes from "./moviesRoutes";
 import reservationRoutes from "./reservationRoutes";
+import paymentRoutes from "./paymentRoutes";
+import ticketsRoutes from "./ticketsRoutes";
 
 export const router = Router();
 
 router.use("/users", AuthRoutes);
 router.use("/movies", moviesRoutes);
 router.use("/reserve", reservationRoutes);
+router.use("/payment", paymentRoutes);
+router.use("/tickets", ticketsRoutes);
 
 export default router;
