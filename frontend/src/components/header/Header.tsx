961385388a7e4b23c64173d928cc092096f6d96e
juan_pablo_alvarez <juanpabloalvarezovalle@gmail.com>
2025-04-15 16:53:04 -0500

    Fixed header for responsiveness and title chnaged

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,25 +1,47 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Typography, useMediaQuery } from "@mui/material";
 
 import classes from "./Header.module.css";
 import { dinamicImport } from "../../utils/utils";
+import { useContext } from "react";
+import context from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 /**
  * Header of the pages
  */
 const Header = () => {
+  const { userData, logout } = useContext(context);
+  const navigate = useNavigate();
+  const small = useMediaQuery("(max-width: 500px)");
+
+  const logoutHandler = () => {
+    if (userData) {
+      logout();
+      navigate("/");
+    }
+  };
+
   return (
     <Box className={classes["header-container"]}>
       <Box className={classes["header-logo"]}>
         <img src={dinamicImport("logo.webp")} alt="Cinema Logo" />
       </Box>
       <Box>
-        <Typography variant="h3" color="white" fontWeight="800">
-          My cinema
+        <Typography
+          variant={small ? "h5" : "h3"}
+          color="white"
+          fontWeight="800"
+          fontFamily="Kanit"
+        >
+          CineLoop
         </Typography>
       </Box>
       <Box>
-        {/* TODO: implement login logic for using the page as admon */}
-        <Button variant="contained">Login</Button>
+        {userData && (
+          <Button variant="contained" onClick={logoutHandler}>
+            {userData === null ? "Login" : "Logout"}
+          </Button>
+        )}
       </Box>
     </Box>
   );
